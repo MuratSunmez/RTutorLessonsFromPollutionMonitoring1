b1a6250aa9176e63b14dc60a33e2f3e646de3ac8
Murat_Sunmez <muratsunmez24@gmail.com>
2025-04-26 21:10:55 +0200

    Initial commit

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorLessonsFromPollutionMonitoring"
+  
+  # Name of problem sets in the package
+  ps = c("Lessons from Pollution Monitoring in China")
+  
+  list(package=package, ps = ps)
+}
